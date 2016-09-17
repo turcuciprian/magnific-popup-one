51102c302ab13cc6abf93ac89feaacc2e3135b51
Ciprian Turcu <turcuciprian1@gmail.com>
2016-09-17 20:49:14 +0300

    inline scripts + backend functionality implemented

--- magnific-popup-one.php
+++ magnific-popup-one.php
@@ -10,18 +10,28 @@
   Text Domain: aB magnific popup
  */
  if (!function_exists('get_plugins')) {
-
-                  require_once ABSPATH.'wp-admin/includes/plugin.php';
-              }
+     require_once ABSPATH.'wp-admin/includes/plugin.php';
+ }
               $plugin_folder = get_plugins('/'.plugin_basename(dirname(__FILE__)));
               $plugin_file = basename((__FILE__));
-              $dir = plugin_dir_path( __FILE__ );
-              if($dir.'/abExport.php'){
-                require_once 'abExport.php';
+              $dir = plugin_dir_path(__FILE__);
+              if ($dir.'/abExport.php') {
+                  require_once 'abExport.php';
               }
 //magnific popup one enqueue script
- function mpoEnqueue(){
-   wp_enqueue_script('mpoScripts', plugin_dir_url( __FILE__ ) .'src/jquery.magnific-popup.min.js');
-   wp_enqueue_style('mpoStyles', plugin_dir_url( __FILE__ ) .'src/jquery.magnific-popup.css');
+ function mpoEnqueue()
+ {
+     global $abGen;
+     $mpoAffectGallery = $abGen->getField('abOption_cPage_mpo1', 'tab1', 'mpoAffectGallery');
+     if ($mpoAffectGallery) {
+         wp_enqueue_script('jquery');
+         wp_enqueue_script('mpoMagnificScripts', plugin_dir_url(__FILE__).'src/jquery.magnific-popup.min.js', array('jquery'));
+         wp_enqueue_script('mpoCustomScript', plugin_dir_url(__FILE__).'src/script.js', array('jquery', 'mpoMagnificScripts'));
+         $mpoCustImageClass = $abGen->getField('abOption_cPage_mpo1', 'tab1', 'mpoCustImageClass');
+         $inlineScript = "$('.".$mpoCustImageClass."').magnificPopup({type:'image'});";
+         wp_add_inline_script('mpoCustomScript', $inlineScript);
+         wp_enqueue_style('mpoMAgnificStyles', plugin_dir_url(__FILE__).'src/jquery.magnific-popup.css');
+     }
  }
- add_action('wp_enqueue_scripts','mpoEnqueue');
+
+ add_action('wp_enqueue_scripts', 'mpoEnqueue');
