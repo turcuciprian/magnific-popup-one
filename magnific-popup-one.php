2b7a481b1478cba711f3f9d3dfc911f3f72b7ee2
Ciprian Turcu <turcuciprian1@gmail.com>
2016-09-17 13:50:07 +0300

    added magnific popup site url in the description of the plugin

--- magnific-popup-one.php
+++ magnific-popup-one.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Magnific Popup One
   Plugin URI: http://admin-builder.com
-  Description: A plugin that Integrates the magnific popup popup script to your site and makes images customly compatible, supports default wordpress gallery and can be configured for custom images to function.
+  Description: A plugin that Integrates the magnific popup popup script (from http://dimsemenov.com/plugins/magnific-popup/ ) to your site and makes images customly compatible, supports default wordpress gallery and can be configured for custom images to function.
   Version: 1.0
   Author: rootabout
   Author URI: http://admin-builder.com
