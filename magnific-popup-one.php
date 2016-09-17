c299f87de32592c33fe0a9716a26d20568f36505
Ciprian Turcu <turcuciprian1@gmail.com>
2016-09-17 13:39:41 +0300

    added main plugin file and changed default commented text

---
+++ magnific-popup-one.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: Magnific Popup One
+  Plugin URI: http://admin-builder.com
+  Description: A plugin that Integrates the magnific popup popup script to your site and makes images customly compatible, supports default wordpress gallery and can be configured for custom images to function.
+  Version: 1.0
+  Author: rootabout
+  Author URI: http://admin-builder.com
+  License: GPLv2 or later
+  Text Domain: aB magnific popup
+ */
